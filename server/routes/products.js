29e33febe731a6f959c44544f14b50af59e4e139
Tushar-Biswas <tusharbiswas1014gmail.com>
2023-03-13 01:19:53 +0600

    schema & resolvers added

---
+++ server/routes/products.js
@@ -0,0 +1,103 @@
+const express = require('express');
+const router = express.Router();
+const Product = require('../models/Product');
+
+// GET all products
+router.get('/', async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// GET a product by ID
+router.get('/:id', async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      return res.status(404).json({ msg: 'Product not found' });
+    }
+    res.json(product);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Product not found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
+
+// CREATE a product
+router.post('/', async (req, res) => {
+  const { name, description, price, image } = req.body;
+
+  try {
+    const product = new Product({
+      name,
+      description,
+      price,
+      image,
+    });
+
+    await product.save();
+
+    res.json(product);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// UPDATE a product by ID
+router.put('/:id', async (req, res) => {
+  const { name, description, price, image } = req.body;
+
+  try {
+    let product = await Product.findById(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({ msg: 'Product not found' });
+    }
+
+    product.name = name;
+    product.description = description;
+    product.price = price;
+    product.image = image;
+
+    await product.save();
+
+    res.json(product);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Product not found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
+
+// DELETE a product by ID
+router.delete('/:id', async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({ msg: 'Product not found' });
+    }
+
+    await product.remove();
+
+    res.json({ msg: 'Product removed' });
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Product not found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
+
+module.exports = router;
