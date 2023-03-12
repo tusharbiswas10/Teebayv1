258a25949dea8cb07a1a6bd93acf23fb479edef4
Tushar-Biswas <tusharbiswas1014gmail.com>
2023-03-12 19:58:14 +0600

    Login, signup pages added

---
+++ client/src/pages/SignupPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/Header';
+import SignupForm from '../components/SignupForm';
+
+const SignupPage = () => {
+  return (
+    <>
+      <Header />
+      <SignupForm />
+    </>
+  );
+};
+
+export default SignupPage;
