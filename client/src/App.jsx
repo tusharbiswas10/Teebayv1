4adaae815a81b4fa324158f68ce81a5f739d77ac
Tushar-Biswas <tusharbiswas1014gmail.com>
2023-03-12 23:36:21 +0600

    backend added &  bug fix in frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,16 +5,16 @@ import { ApolloProvider } from '@apollo/client';
 import client from './graphql/client';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
-import MyProductsPage from './pages/MyProductspage';
+//import MyProductsPage from './pages/MyProductspage';
 
 const App = () => {
   return (
     <ApolloProvider client={client}>
       <Router>
         <Routes>
-          <Route exact path="/" component={LoginPage} />
-          <Route exact path="/signup" component={SignupPage} />
-          <Route exact path="/my-products" component={MyProductsPage} />
+          <Route exact path="/" element={<LoginPage/>} />
+          <Route exact path="/signup" element={<SignupPage/>} />
+          
         </Routes>
       </Router>
     </ApolloProvider>
