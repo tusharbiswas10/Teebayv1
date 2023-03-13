ebf5bd65137ae45f574c4fa8818b24e046dc9a2a
Tushar-Biswas <tusharbiswas1014gmail.com>
2023-03-14 00:48:49 +0600

    bug fix

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 // Middlewares
 app.use(cors());
